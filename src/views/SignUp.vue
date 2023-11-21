cb29fdde066413b87267e30216a9c641c983f55d
Mordecai Stanhope <ab.abdalla@hotmail.com>
2023-11-21 20:39:53 +0400

    feat: Add Sign-up component

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,18 +1,14 @@
 <template>
-  <fwb-alert type="success" v-if="showSuccessAlert">
-    Your form is submitted successfully!
-  </fwb-alert>
-  <fwb-input v-model="name" label="Name" />
-  <fwb-select v-model="selected" :options="league" label="League" />
-  <fwb-toast divide v-if="selected"> Sign up will end on {{ getEndDate(selected) }}. </fwb-toast>
-  <fwb-button gradient="red" @click="submitForm">Confirm</fwb-button>
+  <fwb-alert type="success" v-if="showSuccessAlert"> "Zostałeś zapisany!" </fwb-alert>
+  <fwb-select v-model="selected" :options="league" label="Liga" />
+  <fwb-toast divide v-if="selected">Zapisy zakończą się {{ getEndDate(selected) }}.</fwb-toast>
+  <fwb-button gradient="red" @click="submitForm">Zapisz się</fwb-button>
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue'
-import { FwbInput, FwbSelect, FwbToast, FwbButton, FwbAlert } from 'flowbite-vue'
+import { FwbSelect, FwbToast, FwbButton, FwbAlert } from 'flowbite-vue'
 
-const name = ref('')
 const selected = ref('')
 const showSuccessAlert = ref(false)
 
@@ -21,8 +17,7 @@ const league = [
   { value: 'tk', name: 'Tekken 8', endDate: '03-12-2022 08:30 GMT' },
   { value: 'gg', name: 'Guilty Gear Xrd', endDate: '20-05-2023 18:00 GMT' }
 ]
-
-const getEndDate = (selectedValue: string) => {
+function getEndDate(selectedValue: string): string {
   const selectedLeague = league.find((league) => league.value === selectedValue)
   return selectedLeague ? selectedLeague.endDate : ''
 }
