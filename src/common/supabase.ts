eadccbce8614bc8f68e86742a004f70b7147d44b
TuscanPL <okarinpl@gmail.com>
2023-11-28 20:45:02 +0100

    feat: Add project settings

--- src/common/supabase.ts
+++ src/common/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
+import { envConfig } from './envVariables'
 
-const supabaseUrl = "https://szdfoftdifsvtiacvxuw.supabase.co"
-const supaBasePublicKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6ZGZvZnRkaWZzdnRpYWN2eHV3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM0OTQwOTMsImV4cCI6MjAwOTA3MDA5M30.tu_X6AB2_K8UnFKxUcOs_ksdypkyYOXrnTJyXIBrh4A"
+const supabaseUrl = envConfig.supabaseUrl ?? ''
+const supaBasePublicKey = envConfig.supabasePublicKey ?? ''
 
 export const supabase = createClient(supabaseUrl, supaBasePublicKey)
--- src/main.ts
+++ src/main.ts
@@ -5,17 +5,17 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import * as LineAwesome from "oh-vue-icons/icons/la";
+import * as LineAwesome from 'oh-vue-icons/icons/la'
 import { OhVueIcon, addIcons } from 'oh-vue-icons'
 
 const icons = Object.values({ ...LineAwesome })
-addIcons(...icons);
+addIcons(...icons)
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 
-app.component("v-icon", OhVueIcon);
+app.component('v-icon', OhVueIcon)
 
 app.mount('#app')
