d56f8d461c6954313a31335206e91465b4df67e9
Mordecai Stanhope <ab.abdalla@hotmail.com>
2023-11-29 16:45:36 +0400

    feature: login-callback-timeout-and-spinner

--- src/views/LoginCallback.vue
+++ src/views/LoginCallback.vue
@@ -1,20 +1,36 @@
 <template>
-  <div>You made it into the callback section</div>
+  <div class="flex flex-col justify-center items-center h-screen">
+    <fwb-spinner size="10" color="red" />
+    <div class="text-lg">Getting User Data</div>
+  </div>
 </template>
 <script setup lang="ts">
 import { onMounted } from 'vue'
 import { useUserStore } from '@/stores/userStore'
 import { useRouter } from 'vue-router'
 import { routerPaths } from '@/router/routes'
+import { FwbSpinner } from 'flowbite-vue'
 
 const userStore = useUserStore()
 const router = useRouter()
 
-onMounted(() => {
+onMounted(async () => {
   if (!userStore.isCookiePresent) {
-    window.location.reload()
+    await waitForCookie()
   }
 
   router.push(routerPaths.home)
 })
+
+async function waitForCookie() {
+  const timeout = 500
+  const start = Date.now()
+
+  while (!userStore.isCookiePresent) {
+    if (Date.now() - start > timeout) {
+      return window.location.reload()
+    }
+    await new Promise((resolve) => setTimeout(resolve, 50))
+  }
+}
 </script>
